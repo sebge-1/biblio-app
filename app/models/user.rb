43149c1674f78738dddad42a4bbf60793fb13aed
Sebastian <sebgertz@gmail.com>
2018-04-10 07:17:31 +0100

    user validations for username and password

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,11 @@
 class User  < ActiveRecord::Base
 
   has_many :booknotes
-  has_many :book, through: :booknotes
+  has_many :books, through: :booknotes
+
+  validates :username, presence: {"Mmh, looks like you missed something here."}
+  validates :username, uniqueness: {"Aw, that one's gone. Try another."}
+  validates :password, presence: {"Better fill that in."}
 
   has_secure_password
 end
