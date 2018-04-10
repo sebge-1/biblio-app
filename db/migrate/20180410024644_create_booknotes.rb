e21839e7c2aa53d0a56c1498124fd7434f5ece42
Sebastian <sebgertz@gmail.com>
2018-04-10 03:54:46 +0100

    stubbed out booknotes table

--- db/migrate/20180410024644_create_booknotes.rb
+++ db/migrate/20180410024644_create_booknotes.rb
@@ -1,4 +1,11 @@
 class CreateBooknotes < ActiveRecord::Migration
   def change
+    create_table :booknotes do |t|
+      t.string :title
+      t.string :main_premise
+      t.string :quotes
+      t.string :follow_up
+      t.integer :user_id
+      t.integer :book_id
   end
 end
