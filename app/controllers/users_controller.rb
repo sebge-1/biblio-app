2b277643d87f3599d43187fd8178d844fed0253f
Sebastian <sebgertz@gmail.com>
2018-04-11 04:12:18 +0100

    login flow displays error message & works

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -21,6 +21,23 @@ class UsersController < ApplicationController
     redirect '/'
   end
 
+  post '/login' do
+    @user = User.find_by(username: params[:username])
+    if @user && @user.authenticate(params[:password])
+      session[:id] = @user.id
+      redirect '/users/:id'
+    else
+      if params[:username].blank? && params[:password].blank?
+        @error_message = "Please enter something."
+      elsif @user.nil?
+        @error_message = "We don't seem to know about you."
+      elsif !@user.authenticate(params[:password])
+        @error_message = "Wrong password. Try again."
+      end
+      erb :index
+    end
+  end
+
   get '/users/:id' do
     @user = current_user
     erb :'/users/show'
