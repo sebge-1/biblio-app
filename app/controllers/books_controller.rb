d7dc3977172fc95b188b35749b69aeeb59c5a766
Sebastian <sebgertz@gmail.com>
2018-06-08 23:03:58 -0400

    basic user show page and book create method working

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,4 +4,15 @@ class BooksController < ApplicationController
     erb :'/books/new'
   end
 
+  post '/books' do
+    @user = current_user
+    if !params[:title].empty? && !params[:author].empty? #don't allow empty zombie books
+      @book = Book.new(title: params[:title], author: params[:author])
+      @user.books << @book
+      redirect "/users/#{@user.id}"
+    else
+      redirect '/books/new'
+    end
+  end
+
 end
