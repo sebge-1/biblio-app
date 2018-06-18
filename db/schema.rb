bb3b89552c01de88df7caa37522e8ea079a674b9
Sebastian <sebgertz@gmail.com>
2018-06-18 12:15:05 -0400

    db migrated

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,34 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20180618144523) do
+
+  create_table "authors", force: :cascade do |t|
+    t.string "name"
+  end
+
+  create_table "book_users", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "book_id"
+  end
+
+  create_table "booknotes", force: :cascade do |t|
+    t.string  "summary"
+    t.string  "quotes"
+    t.string  "research"
+    t.integer "book_id"
+  end
+
+  create_table "books", force: :cascade do |t|
+    t.string  "title"
+    t.string  "author"
+    t.integer "author_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "email"
+    t.string "password_digest"
+  end
 
 end
